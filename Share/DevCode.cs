e53c6640994d7d7f722a4cbb68520831c4c7d6d2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-06-27 21:15:20 +0630

    -

--- Share/DevCode.cs
+++ Share/DevCode.cs
@@ -8,7 +8,8 @@ namespace Share
 {
     public static class DevCode
     {
-        public static IQueryable<TSource> WithPagination<TSource>(this IQueryable<TSource>source,int pageNo,int pageSize)
+        public static IQueryable<TSource> WithPagination<TSource>
+            (this IQueryable<TSource>source,int pageNo,int pageSize)
         {
             return source
                 .Skip((pageNo - 1) * pageSize)
