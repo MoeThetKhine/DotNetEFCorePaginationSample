f212e28bd498706ced5eea6874d1c3a6a7b30e34
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 02:10:25 +0630

    Format Code

--- DotNetEFCorePaginationSample/Features/BaseController.cs
+++ DotNetEFCorePaginationSample/Features/BaseController.cs
@@ -1,21 +1,20 @@
-﻿namespace PresentationLayerApi.Features
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
-    {
-        #region Content
-        protected IActionResult Content(object obj)
-        {
-            return Ok(JsonConvert.SerializeObject(obj));
-        }
-        #endregion
+﻿namespace PresentationLayerApi.Features;
 
-        #region InternalServerError
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
-        #endregion
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    #region Content
+    protected IActionResult Content(object obj)
+    {
+        return Ok(JsonConvert.SerializeObject(obj));
     }
+    #endregion
+
+    #region InternalServerError
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
+    }
+    #endregion
 }
