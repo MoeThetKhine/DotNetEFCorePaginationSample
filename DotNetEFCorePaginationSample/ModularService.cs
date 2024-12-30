da2f28f097980db234e733e236ffae9b35588796
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 02:09:22 +0630

    Format Code

--- DotNetEFCorePaginationSample/ModularService.cs
+++ DotNetEFCorePaginationSample/ModularService.cs
@@ -1,56 +1,55 @@
-﻿namespace PresentationLayerApi
+﻿namespace PresentationLayerApi;
+
+public static class ModularService
 {
-    public static class ModularService
+    #region AddFeatures
+    public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        #region AddFeatures
-        public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContextService(builder);
-            services.AddDataAccess();
-            services.AddBusinessLogic();
-            services.AddJsonService();
-            return services;
-        }
-        #endregion
-
-        #region AddDbContextService
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            });
-            return services;
-        }
-        #endregion
-
-        #region AddDataAccess
-        private static IServiceCollection AddDataAccess(this IServiceCollection services)
-        {
-            services.AddScoped<DA_Blog>();
-            return services;
-        }
-
-        #endregion
-
-        #region AddBusinessLogic
-        private static IServiceCollection AddBusinessLogic(this IServiceCollection services)
-        {
-            services.AddScoped<BL_Blog>();
-            return services;
-        }
-        #endregion
-
-        #region AddJsonService
-        private static IServiceCollection AddJsonService(this IServiceCollection services)
-        {
-            services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            return services;
-        }
-        #endregion
+        services.AddDbContextService(builder);
+        services.AddDataAccess();
+        services.AddBusinessLogic();
+        services.AddJsonService();
+        return services;
     }
+    #endregion
+
+    #region AddDbContextService
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        });
+        return services;
+    }
+    #endregion
+
+    #region AddDataAccess
+    private static IServiceCollection AddDataAccess(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Blog>();
+        return services;
+    }
+
+    #endregion
+
+    #region AddBusinessLogic
+    private static IServiceCollection AddBusinessLogic(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>();
+        return services;
+    }
+    #endregion
+
+    #region AddJsonService
+    private static IServiceCollection AddJsonService(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(opt =>
+        {
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        return services;
+    }
+    #endregion
 }
