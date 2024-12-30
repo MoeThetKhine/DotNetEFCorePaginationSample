dc29ff0899a8a52c4409b7ec04a10560cbaf8d9d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 02:13:29 +0630

    Format Code

--- DotNetEFCorePaginationSample/Features/Blog/BL_Blog.cs
+++ DotNetEFCorePaginationSample/Features/Blog/BL_Blog.cs
@@ -1,21 +1,20 @@
-﻿namespace PresentationLayerApi.Features.Blog
+﻿namespace PresentationLayerApi.Features.Blog;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly DA_Blog _dA_Blog;
+    public BL_Blog(DA_Blog dA_Blog)
     {
-        private readonly DA_Blog _dA_Blog;
-        public BL_Blog(DA_Blog dA_Blog)
-        {
-            _dA_Blog = dA_Blog;
-        }
-        #region GetBlogs
-        public async Task<BlogListResponseModel> GetBlogs(int pageNo, int pageSize)
-        {
-            if (pageNo <= 0)
-                throw new Exception("Page No is Invalid");
-            if (pageSize <= 0)
-                throw new Exception("Page Size is Invalid");
-            return await _dA_Blog.GetBlogs(pageNo, pageSize);
-        }
-        #endregion
+        _dA_Blog = dA_Blog;
     }
+    #region GetBlogs
+    public async Task<BlogListResponseModel> GetBlogs(int pageNo, int pageSize)
+    {
+        if (pageNo <= 0)
+            throw new Exception("Page No is Invalid");
+        if (pageSize <= 0)
+            throw new Exception("Page Size is Invalid");
+        return await _dA_Blog.GetBlogs(pageNo, pageSize);
+    }
+    #endregion
 }
