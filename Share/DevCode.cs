daf6a6fe40482e575c1ff20bd1b854c68e5ec5f1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 01:38:40 +0630

    -

--- Share/DevCode.cs
+++ Share/DevCode.cs
@@ -4,8 +4,7 @@
 
 public static class DevCode
 {
-    public static IQueryable<TSource> WithPagination<TSource>
-        (this IQueryable<TSource>source,int pageNo,int pageSize)
+    public static IQueryable<TSource> WithPagination<TSource>(this IQueryable<TSource>source,int pageNo,int pageSize)
     {
         return source
             .Skip((pageNo - 1) * pageSize)
