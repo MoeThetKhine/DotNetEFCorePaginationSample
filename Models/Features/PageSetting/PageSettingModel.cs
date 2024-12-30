dac051bbc7158850bf453b61fb9b435567293e93
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 01:32:57 +0630

    Format Code

--- Models/Features/PageSetting/PageSettingModel.cs
+++ Models/Features/PageSetting/PageSettingModel.cs
@@ -1,22 +1,21 @@
-﻿namespace Models.Features.PageSetting
+﻿namespace Models.Features.PageSetting;
+
+public class PageSettingModel
 {
-    public class PageSettingModel
+    public int PageNo { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public int TotalCount {  get; set; }
+    public int PageCount { get; set; }
+    public bool IsEndOfPage { get { return PageCount == PageNo; } }
+
+    #region PageSettingModel
+    public PageSettingModel(int pageNo, int pageSize, int totalCount, int pageCount)
     {
-        public int PageNo { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
-        public int TotalCount {  get; set; }
-        public int PageCount { get; set; }
-        public bool IsEndOfPage { get { return PageCount == PageNo; } }
-
-        #region PageSettingModel
-        public PageSettingModel(int pageNo, int pageSize, int totalCount, int pageCount)
-        {
-            PageNo = pageNo;
-            PageSize = pageSize;
-            TotalCount = totalCount;
-            PageCount = pageCount;
-        }
-        #endregion
-
+        PageNo = pageNo;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+        PageCount = pageCount;
     }
+    #endregion
+
 }
