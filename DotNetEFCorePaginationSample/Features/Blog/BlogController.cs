0c64f3bf7ec9ae0d97307e0b1880452345cd3372
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 02:12:46 +0630

    Format Code

--- DotNetEFCorePaginationSample/Features/Blog/BlogController.cs
+++ DotNetEFCorePaginationSample/Features/Blog/BlogController.cs
@@ -1,32 +1,31 @@
-﻿namespace PresentationLayerApi.Features.Blog
+﻿namespace PresentationLayerApi.Features.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
-
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-        #region HttpGet
-
-        [HttpGet("{pageNo}/{pageSize}")]
-        public async Task<IActionResult> GetBlogs(int pageNo, int pageSize)
-        {
-            try
-            {
-                var lst = await _bL_Blog.GetBlogs(pageNo, pageSize);
-                return Content(lst);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
-        }
-
-        #endregion
-
+        _bL_Blog = bL_Blog;
     }
+    #region HttpGet
+
+    [HttpGet("{pageNo}/{pageSize}")]
+    public async Task<IActionResult> GetBlogs(int pageNo, int pageSize)
+    {
+        try
+        {
+            var lst = await _bL_Blog.GetBlogs(pageNo, pageSize);
+            return Content(lst);
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
+
+    #endregion
+
 }
