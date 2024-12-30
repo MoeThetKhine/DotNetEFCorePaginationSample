c036dd9306dbd43a5220bf5f16502bd180ef3728
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 01:37:16 +0630

    -

--- Models/Features/Blog/BlogListResponseModel.cs
+++ Models/Features/Blog/BlogListResponseModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace Models.Features.Blog;
 
+#region BlogListResponseModel
+
 public class BlogListResponseModel
 {
     public List<BlogModel> Blogs {  get; set; }
     public PageSettingModel PageSetting { get; set; }
 }
+
+#endregion
